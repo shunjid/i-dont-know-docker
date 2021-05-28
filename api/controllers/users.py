2d55113da958801ca52f7151e5121bff79179033
shunjid <shunjid.rahman@gmail.com>
2021-05-28 20:59:30 +0600

    separated constant error messages

--- api/controllers/users.py
+++ api/controllers/users.py
@@ -5,6 +5,7 @@ from utils.response import success, error, success_dict
 from schema.user import UserSchema
 from services.user_queries import findAll, findOne, findByEmail, findByMobile
 from services.commands import create_one
+from constants.messages import ERRORS
 
 users = Blueprint("users", __name__)
 user_schema = UserSchema()
@@ -13,17 +14,28 @@ user_schema = UserSchema()
 @users.route("/api/users", methods=["GET"])
 def get_users():
     try:
-        return success(data=findAll(), single=False)
+        return success(
+            data=findAll(),
+            single=False,
+        )
     except Exception as e:
-        return error(message=str(e), status_code=500)
+        return error(
+            message=str(e),
+            status_code=404,
+        )
 
 
 @users.route("/api/users/<id>", methods=["GET"])
 def get_user_by_id(id):
     try:
-        return success(data=findOne(id))
+        return success(
+            data=findOne(id),
+        )
     except Exception as e:
-        return error(message="User not found", status_code=404)
+        return error(
+            message=ERRORS["USER_NOT_FOUND"],
+            status_code=404,
+        )
 
 
 @users.route("/api/users", methods=["POST"])
@@ -43,20 +55,27 @@ def create_user():
 
         if findByEmail(new_user.email_address):
             return error(
-                message="User already exists with this email",
+                message=ERRORS["EMAIL_EXISTS"],
                 status_code=500,
             )
         elif findByMobile(new_user.mobile_number):
             return error(
-                message="User already exists with this mobile",
+                message=ERRORS["MOBILE_EXISTS"],
                 status_code=500,
             )
         else:
             create_one(new_user)
-            return success_dict(data=schema)
 
+            return success_dict(
+                data=schema,
+            )
     except ValidationError as err:
-        return error(message=err.messages, status_code=500)
-
+        return error(
+            message=err.messages,
+            status_code=500,
+        )
     except Exception as e:
-        return error(message=str(e), status_code=500)
+        return error(
+            message=str(e),
+            status_code=500,
+        )
