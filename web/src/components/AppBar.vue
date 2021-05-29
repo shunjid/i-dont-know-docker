a241a96383b24c66456cfb53885457ee08529ab6
shunjid <shunjid.rahman@gmail.com>
2021-05-29 22:17:15 +0600

    Set vue initial with vuetify

---
+++ web/src/components/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <h1 class="primaryFont">Contacts</h1>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/shunjid/i-dont-know-docker"
+      target="_blank"
+      text
+    >
+      <span class="mr-2 primaryFont">Repository</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
---
+++ web/src/plugins/vuetify.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+
+import colors from "vuetify/lib/util/colors";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.blue.darken3,
+        secondary: colors.pink.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+    },
+  },
+});
