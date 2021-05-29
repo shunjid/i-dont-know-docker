242265741041045cb4ee029aa6a38e4fb201549c
shunjid <shunjid.rahman@gmail.com>
2021-05-29 12:50:54 +0600

    completed delete endpoint

--- api/services/commands.py
+++ api/services/commands.py
@@ -1,10 +1,15 @@
 from services.database import db
 
 
-def create_one(instance):
-    db.session.add(instance)
-    db.session.commit()
-
-
 def commit():
     db.session.commit()
+
+
+def create_one(instance):
+    db.session.add(instance)
+    commit()
+
+
+def delete_one(instance):
+    db.session.delete(instance)
+    commit()
